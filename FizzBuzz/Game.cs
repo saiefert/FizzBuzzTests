9516fdfa5b3de85a8fbcfb60406eb36ff217b1a3
josma <josma@DESKTOP-F7T2BU7>
2021-05-30 15:52:03 -0400

    Add project files.

---
+++ FizzBuzz/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class Game
+    {       
+        public static string PlayNumber(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+
+            else if (num % 3 == 0) return "Fizz";
+
+            else if (num % 5 == 0) return "Buzz";
+
+            else return $"{num}";
+        }
+    }
+}
---
+++ FizzBuzzTest/GameTest.cs
@@ -0,0 +1,47 @@
+﻿using FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzzTest
+{
+    public class GameTest
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void DivideByThre(int number)
+        {
+            var result = Game.PlayNumber(number);
+            Assert.Equal("Fizz", result);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        public void DivideByFive(int number)
+        {
+            var result = Game.PlayNumber(number);
+            Assert.Equal("Buzz", result);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        public void DivideByThreeFive(int number)
+        {
+            var result = Game.PlayNumber(number);
+            Assert.Equal("FizzBuzz", result);
+        }
+
+        [Fact]
+        public void OtherNumber()
+        {
+            var result = Game.PlayNumber(1);
+            Assert.Equal("1", result);
+        }
+    }
+}
